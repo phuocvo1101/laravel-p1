44842cc880a4adedda03437540c6046c6027a9d0
klord <tanhuan.klord@gmail.com>
2016-05-03 22:23:05 +0700

    PH-011: Fix footer - SĐT

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -10,7 +10,8 @@
                             <br> Đổi xe cũ - sở hữu xe mới chính hãng giá ưu đãi.
                             <br> Dịch vụ hỗ trợ 24/7, Tư vấn lái thử miễn phí.</p>
                         <p>Địa chỉ: {{$infors['addr']}}
-                            <br> Tel: 0933631212. Fax: (84.8) 35126969
+                            <br> Tel: 0933631212. Fax: 0835 12 3939
+                            <br> Hotline dịch vụ: 0944 979797 (24/7)
                             <br> Email: {{$infors['email']}}
                             <br> Website bán hàng: <a href="http://toyotahcm.net.vn">http://toyotahcm.net.vn</a>
                             <br><span style="font-size:8px;">LK: <a href="http://toyotahcm.net.vn">Toyota Vietnam</a>,&nbsp;<a href="http://toyotahcm.net.vn">Toyota C200</a> ,<a href="http://toyotahcm.net.vn">Toyota S400</a> ,<a href="http://toyotahcm.net.vn">Toyota S500</a> ,<a href="http://toyotahcm.net.vn">Toyota GLC</a></span></p>
