4691ecd056351e4bb346c70a70b2665caf9ee521
klord <tanhuan.klord@gmail.com>
2016-08-26 22:49:09 +0700

    PH-015H1: add google tracking

--- resources/views/home/master.blade.php
+++ resources/views/home/master.blade.php
@@ -25,6 +25,22 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <!-- Google tracking -->
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 874970302;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+    <div style="display:inline;">
+    <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/874970302/?value=0&amp;guid=ON&amp;script=0"/>
+    </div>
+    </noscript>
+    <!-- /Google tracking -->
   </head>
 <body>
 
